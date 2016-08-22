bd100e2a33b69df3de2d6eba5d85bdcb819655a7
Park_JaeHyun <wogus6523>
2016-08-23 01:41:24 +0900

    Rejected commit: Default

--- app/src/main/java/com/example/park_jaehyun/uihomework/MainActivity.java
+++ app/src/main/java/com/example/park_jaehyun/uihomework/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.park_jaehyun.uihomework;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout drawer;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+
+        Button btn = (Button)findViewById(R.id.btn_navi);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                getSupportActionBar().setHomeAsUpIndicator(android.R.drawable.ic_dialog_info);
+                if (savedInstanceState == null) {
+                    getSupportFragmentManager().beginTransaction()
+                            .add(R.id.container, MessageFragment.newInstance("Main"))
+                            .commit();
+                }
+
+            }
+        });
+
+        Button btn1 = (Button)findViewById(R.id.btn_risk);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RickCallActivity.class));
+            }
+        });
+
+        Button btn2 = (Button)findViewById(R.id.btn_add);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SetProtectorActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/park_jaehyun/uihomework/MenuChild.java
@@ -0,0 +1,14 @@
+package com.example.park_jaehyun.uihomework;
+
+/**
+ * Created by Administrator on 2016-07-27.
+ */
+public class MenuChild {
+    public String menuName;
+    public int menuId;
+
+    public MenuChild(String menuName, int menuId) {
+        this.menuName = menuName;
+        this.menuId = menuId;
+    }
+}
